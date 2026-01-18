b833fd07364810946330038fecf3517f27f6f1ac
cleybson7 <cleybsonsouza5@gmail.com>
2026-01-18 14:27:04 -0300

    base

---
+++ first-redis-project/src/main/java/com/initial/first_redis_project/FirstRedisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.initial.first_redis_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRedisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRedisProjectApplication.class, args);
+	}
+
+}
---
+++ first-redis-project/src/test/java/com/initial/first_redis_project/FirstRedisProjectApplicationTests.java
@@ -0,0 +1,29 @@
+package com.initial.first_redis_project;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+class FirstRedisProjectApplicationTests {
+
+    @Autowired
+    private ReactiveStringRedisTemplate template;
+
+    @RepeatedTest(3)
+	void springDataRedisTest() {
+        ReactiveValueOperations<String, String> valueOperations = this.template.opsForValue();
+        Mono<Void> mono = Flux.range(1, 500_000)
+                .flatMap(i -> valueOperations.increment("user:1:visit"))
+                .then();
+        StepVerifier.create(mono)
+                .verifyComplete();
+	}
+
+}
